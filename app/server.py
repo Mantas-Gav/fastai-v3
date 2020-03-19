d707fa954a86b7add0272d70bd07209f5f126d78
s2006391 <m.gavenavicius@student.utwente.nl>
2020-03-19 15:32:05 +0100

    trying to change the json

--- app/server.py
+++ app/server.py
@@ -61,7 +61,17 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
-    return JSONResponse({'class': str(prediction[0]), 'raw': str(prediction[2])})
+	raw = prediction[2]
+    return JSONResponse({
+		'main_result': str(prediction[0]), 
+		'emotions': {
+		# goes like [angry, happy, neutral, sad] in the tensor
+			"anger" : str(raw[0]),
+			"neutral" : str(raw[2]),
+			"happiness" : str(raw[1]),
+			"sadness" : str(raw[3])
+		}
+	})
 
 
 if __name__ == '__main__':
